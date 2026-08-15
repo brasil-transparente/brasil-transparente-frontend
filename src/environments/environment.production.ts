0128ce4c10eef978876d616e806efef41b641b3b
gabrielmissel <gabrielmissel@pm.me>
2026-08-15 16:40:37 -0300

    Melhorando llms e urls

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiBase: 'https://brasil-transparente-api-492194254445.us-central1.run.app',
+  apiBase: 'https://api.brasiltransparente.digital',
   shouldCache: true,
   siteUrl: 'https://brasiltransparente.digital'
 };
